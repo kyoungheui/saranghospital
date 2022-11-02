ad15e56799e4ccd5df666167aa5c8366a4d9349d
kyoungheui <103475194+kyoungheui@users.noreply.github.com>
2022-11-02 13:39:00 +0900

    11월2일

---
+++ js/slide.js
@@ -0,0 +1,31 @@
+const centerOverflow = document.querySelector('.center-overflow')
+const slideUlcontainer = centerOverflow.children[0];
+const slideGap = 20;
+const slideWidth = centerOverflow.children[0].children[0].offsetWidth;
+const slideValue = slideGap + slideWidth;
+
+const pB = document.getElementById('pb');
+const bb = document.getElementById('bb');
+
+pB.addEventListener('click', prevSlide)
+bb.addEventListener('click', nextSlide)
+
+let index = 0;
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = 0;
+    }
+    console.log(index);
+    slideUlcontainer.style.transform = `translateX(-${index*slideValue}px)`
+}
+
+function nextSlide() {
+    index++;
+    if (index > slideUlcontainer.childElementCount - 6) {
+        index = 8;
+    }
+    console.log(index);
+    slideUlcontainer.style.transform = `translateX(-${index*slideValue}px)`
+}
